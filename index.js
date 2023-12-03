03e0ec6fa26974b62475958c86eab9cc983c2105
DappyNetwork <92771259+Dappy-Net@users.noreply.github.com>
2023-12-03 09:52:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
     res.render('index', { title: 'My Express App' });
 });
+
+app.get('*', (req, res) => {
+  res.render('404', { title: '404' });
+});
+
+app.use('/assets', express.static('./assets'))
+
 app.listen(3000, () => {
     console.log('Server started on port 3000');
 });
 
-app.post('/display-input', (req, res) => {
-    const input = req.body.hello;
-    res.render('input', { input });
-});
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
-app.get('/input', (req, res) => {
-    res.render('input');
-});
+
